dbb9c6dcfeadd1078baf03444f40209906934831
Cookzz <souyuuki12@gmail.com>
2018-05-13 01:23:55 +0800

    Style Updates
    Changes:
    - New font added
    - New colors added
    - Login & Sign Up Forms styles updated
    - Added new pictures and animation
    - Removed excess code/pictures
    - Webscript update (fade out animation for modal)
    - Others (navigation bar)

--- application/views/templates/popupforms.php
+++ application/views/templates/popupforms.php
@@ -6,14 +6,16 @@
             <span onclick="document.getElementById('loginForm').style.display='none'" class="close" title="Close Modal">&times;</span>
             <form class="modal-content animate" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
                 <div class="container">
-                    <label><b>User ID</b></label>
+                   <center><img src="<?php echo base_url(); ?>assets/images/FFF_Logo.png" class="loginFormPic">
+                       <p class="loginTitle">LOGIN INFO</p>
+                    <label><h5>User ID</h5></label>
                     <input id="user_id_field" type="text" placeholder="Enter User ID e.g: 16123456" name="user_id" required>
+                    
 
-                    <label><b>Password</b></label>
+                    <label><h5>Password</h5></label></center>
                     <input id="password_field" type="password" placeholder="Enter Password e.g: @abc123" name="password" required>
                     <label id="errorMessage"></label>
-                    <br>
-                    <input type="checkbox" checked="checked">Remember Username & Password
+                    <label class="rememberLabel"><input type="checkbox" checked="checked" class="remember">Remember Username & Password</label>
 
                     <div class="clearfix">
                         <button type="button" onclick="document.getElementById('loginForm').style.display='none'" class="cancelbtn">Cancel</button>
@@ -29,12 +31,14 @@
             <span onclick="document.getElementById('forgotForm').style.display='none'" class="close" title="Close Modal">&times;</span>
             <form class="modal-content animate" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
                 <div class="container">
-                    
-                    <label><b>Email Address</b></label>
+                   <span><button type="button" onclick="signLogin(0)" class="backbtn"><img src="<?php echo base_url(); ?>assets/images/backarrow.png" class="backarrowbtn"></button></span>
+                    <center><img src="<?php echo base_url(); ?>assets/images/lockpicture.png" class="forgotPassPic">
+                    <h2 class="forgotPassTitle">Forgot Password?</h2>
+                        <p>Just type in your email address and we will email a new password for you</p>
+                    <label><h5>Email Address</h5></label></center>
                     <input type="text" placeholder="Enter Email e.g: abc123@gmail.com" name="email" required>
 
                     <div class="clearfix">
-                        <button type="button" onclick="signLogin(0)" class="cancelbtn">Go Back</button>
                         <button type="submit" class="forgotbtn" name="forgot" value="Submit">Confirm</button>
                     </div>
                 </div>
