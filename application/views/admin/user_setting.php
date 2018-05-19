0c42a583214d88807274ff39aa6b10195176274c
Cookzz <souyuuki12@gmail.com>
2018-05-20 04:30:09 +0800

    Profile settings style update
    Changes:
    - Profile picture position moved to top
    - Moved "choose file" with the profile picture
    - Created "sections" for profile settings
    - Added styling to texts

--- application/views/admin/user_setting.php
+++ application/views/admin/user_setting.php
@@ -6,13 +6,24 @@
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/user_setting.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css">
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/adminUserSettingStyle.css">
 
 </head>
 <body>  
 
-<h2>User Setting</h2><hr/>
+<h2 class="pageTitle">User Setting</h2><hr class="pageTitleHr">
 
 <table>
+   <tr>
+       <td class="profPicRow"><label class="profPicLabel">Profile Picture</label><hr></td>
+   </tr>
+    <tr>
+		<td><img class="profilePicture" src="<?php echo base_url();?>assets/images/<?= $user_data["user_picture"]?>" width="100px"></td>
+	</tr>
+	<tr><td><input id="newImage" type="file" accept="image/*" name="image" onchange="loadImage(event)"><hr></td></tr>
+	<tr>
+	    <td><label class="settingLabel">Other Settings</label><hr class="otherHr"></td>
+	</tr>
 	<tr>
 		<td>User ID</td>
 		<td><input type="text" disabled="true" value="<?= $user_data["user_id"]?>"></td>
@@ -29,15 +40,13 @@
 		<td>User Position</td>
 		<td><input type="text" disabled="true" value="<?= $user_data["user_position"]?>"></td>
 	</tr>
-	<tr>
-		<td>User Picture</td>
-		<td><img src="<?php echo base_url();?>assets/images/<?= $user_data["user_picture"]?>" width="100px"></td>	
-	</tr>
+   <tr>
+       <td><hr></td>
+   </tr>
     </table>
 <br />
 <div>
-<h5>Upload New Profile Picture</h5>
-<input id="newImage" type="file" accept="image/*" name="image" onchange="loadImage(event)">
+
 <br /><img id="output" width="300px"><button id="confirmUpload" style="display:none">Confirm Upload</button>
 </div>
 
