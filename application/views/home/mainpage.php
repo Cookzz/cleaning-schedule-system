174bffeb4f7e055806e24ef248993f2541152cd3
Cookzz <souyuuki12@gmail.com>
2018-06-03 03:31:43 +0800

    Style update
    Changes:
    - Cleaner schedule now tabbed
    - Minor style changes

--- application/views/home/mainpage.php
+++ application/views/home/mainpage.php
@@ -1,18 +1,22 @@
 <!DOCTYPE HTML>  
 <html>
 <head>
-
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/mainPageStyle.css">
 </head>
 <body>  
 
 <h2>Main Page</h2><hr/>
 
-<?php foreach($special_duties as $special_duty):?>
-<h3><?= $special_duty["special_duty_title"]?></h3>
-<h6>Date : <?= $special_duty["special_duty_date"]?></h6><h6>Time : <?= $special_duty["special_duty_time"]?></h6>
-<?= $special_duty["special_duty_detail"]?>
-<hr />
-<?php endforeach;?>
+<div class="mainPage">
+    <?php foreach($special_duties as $special_duty):?>
+    <h3><?= $special_duty["special_duty_title"]?></h3>
+    <h6><label>Date:</label> <?= $special_duty["special_duty_date"]?></h6>
+    <h6><label>Time:</label> <?= $special_duty["special_duty_time"]?></h6>
+    <h6><label>Details:</label> <?= $special_duty["special_duty_detail"]?></h6>
+    <hr />
+    <?php endforeach;?>
+</div>
+
 
 </body>
 </html>
