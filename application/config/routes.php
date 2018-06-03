f75bcc8661382e83f7445a5686d73e62112ab87f
derwinsim <38890210+derwinsim@users.noreply.github.com>
2018-06-03 23:53:57 +0800

    set base url default access

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,6 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-$route['default_controller'] = 'HomeController/viewHomePage';
+$route['default_controller'] = 'HomeController/viewMainPage';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
