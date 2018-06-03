63e6fdedfae7489d03b0439802fdbd73e07f7616
derwinsim <38890210+derwinsim@users.noreply.github.com>
2018-06-04 00:44:02 +0800

    some bug solve

--- application/views/supervisor/view_pending_duty.php
+++ application/views/supervisor/view_pending_duty.php
@@ -21,51 +21,53 @@
 <a class="viewOutstandingBtn" href="<?php echo base_url().$link;?>"><?= $link_word;?></a>
 <h2 class="pageTitle"><?= $title?></h2>
 <hr/>
-<table id="pending_duty_table" class="table table-striped table-bordered table-responsive" border="1">
-	<thead>
-		<tr class="tableTitle">
-			<th>No</th>
-			<th>Cleaner</th>
-			<th>Task Location</th>
-			<th>Sub Task</th>
-			<th>Comment</th>
-			<th>Time</th>
-			<th>Date</th>
-			<th>Update</th>
-			<th>Delete</th>
-		</tr>
-	</thead>
-	<tbody id="pending_table_body">
-		<?php $i=1;?>
-		<?php foreach($pending_duties as $pending_duty):?>
-		<tr id="<?php echo $pending_duty["pending_duty_id"];?>">
-			<td><?= $i?></td>
-			<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_cleaner"><?php echo $pending_duty["pending_duty_cleaner"];?></td>
-			<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_task"><?php echo $pending_duty["pending_duty_task"];?></td>
-			<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_subtask"><?php echo $pending_duty["pending_duty_subtask"];?></td>
-			<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_comment" contenteditable="true"><?php echo $pending_duty["pending_duty_comment"];?></td>
-			<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_schedule"><?php echo $pending_duty["pending_duty_schedule"];?></td>
-			<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_date"><?php echo $pending_duty["pending_duty_date"];?></td>
-			<td><center><button id="<?php echo $pending_duty["pending_duty_id"];?>update" class="update btn btn-default" type="button">Update</button></center></td>
-			<td><center><button style="width:80px;height:30px" id="<?php echo $pending_duty["pending_duty_id"];?>delete" class="w3-text-red fa fa-trash delete" type="button"></button></center></td>
-		</tr>
-		<?php $i++;?>
-		<?php endforeach; ?>
-	</tbody>
-	<tfoot>
-		<tr>
-			<th>No</th>
-			<th>Cleaner</th>
-			<th>Task Location</th>
-			<th>Sub Task</th>
-			<th>Comment</th>
-			<th>Time</th>
-			<th>Date</th>
-			<th>Update</th>
-			<th>Delete</th>
-		</tr>
-	</tfoot>
-</table>
+<div class="table-responsive">
+	<table id="pending_duty_table" class="table table-striped table-bordered" border="1">
+		<thead>
+			<tr class="tableTitle">
+				<th>No</th>
+				<th>Cleaner</th>
+				<th>Task Location</th>
+				<th>Sub Task</th>
+				<th>Comment</th>
+				<th>Time</th>
+				<th>Date</th>
+				<th>Update</th>
+				<th>Delete</th>
+			</tr>
+		</thead>
+		<tbody id="pending_table_body">
+			<?php $i=1;?>
+			<?php foreach($pending_duties as $pending_duty):?>
+			<tr id="<?php echo $pending_duty["pending_duty_id"];?>">
+				<td><?= $i?></td>
+				<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_cleaner"><?php echo $pending_duty["pending_duty_cleaner"];?></td>
+				<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_task"><?php echo $pending_duty["pending_duty_task"];?></td>
+				<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_subtask"><?php echo $pending_duty["pending_duty_subtask"];?></td>
+				<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_comment" contenteditable="true"><?php echo $pending_duty["pending_duty_comment"];?></td>
+				<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_schedule"><?php echo $pending_duty["pending_duty_schedule"];?></td>
+				<td id="<?php echo $pending_duty["pending_duty_id"];?>_pending_duty_date"><?php echo $pending_duty["pending_duty_date"];?></td>
+				<td><center><button id="<?php echo $pending_duty["pending_duty_id"];?>update" class="update btn btn-default" type="button">Update</button></center></td>
+				<td><center><button style="width:80px;height:30px" id="<?php echo $pending_duty["pending_duty_id"];?>delete" class="w3-text-red fa fa-trash delete" type="button"></button></center></td>
+			</tr>
+			<?php $i++;?>
+			<?php endforeach; ?>
+		</tbody>
+		<tfoot>
+			<tr>
+				<th>No</th>
+				<th>Cleaner</th>
+				<th>Task Location</th>
+				<th>Sub Task</th>
+				<th>Comment</th>
+				<th>Time</th>
+				<th>Date</th>
+				<th>Update</th>
+				<th>Delete</th>
+			</tr>
+		</tfoot>
+	</table>
+</div>
 
 <!--Hidden value to external js file-->
 <input id="baseURL" type=hidden value="<?=base_url()?>">
