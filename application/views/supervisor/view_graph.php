814e6b8c97f478d5d6f6860f727a5610b3b4534e
Cookzz <souyuuki12@gmail.com>
2018-06-16 23:19:42 +0800

    Graph Fix?
    - This change (?) indicate that bugs are not yet found

--- application/views/supervisor/view_graph.php
+++ application/views/supervisor/view_graph.php
@@ -4,6 +4,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/view_graph.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/supervisorGraphStyle.css">
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
 </head>
@@ -23,11 +24,15 @@
 
 <div class="tab-content">
   <div class="tab-pane container active" id="ratingTab">
-    <div id="barRating" class="responsive" style="height: <?= $countOfRating*8?>%; width: 100%;">
+   <div class="graph">
+    <div id="barRating" style="height: <?= $countOfRating*8?>%;">
+    </div>
     </div>
   </div>
   <div class="tab-pane container fade" id="dutiesTab">
-        <div id="pieDuty" class="responsive" style="height:600px; width: 100%;">
+       <div class="graph">
+        <div id="pieDuty">
+        </div>
         </div>
   </div>
 </div>
